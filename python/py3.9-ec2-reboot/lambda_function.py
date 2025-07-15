156e5ec6ead32c4f3d73cecae36c6631486ccf70
bimbap <tama335544tama@gmail.com>
2025-07-15 08:54:08 +0700

    Initial

--- python/py3.9-ec2-reboot/lambda_function.py
+++ python/py3.9-ec2-reboot/lambda_function.py
@@ -1,6 +1,6 @@
 import boto3  
 region = 'us-east-1'
-instances = ['i-0bac2b903e9fff5ea']
+instances = ['i-<YOUR-INSTANCE-ID>']
 ec2 = boto3.client('ec2', region_name=region)
 
 def lambda_handler(event, context):
